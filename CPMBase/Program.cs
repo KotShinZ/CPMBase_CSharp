def253f96020054974b4551aeda54e98e1326bba
KotShinZ <s18574s18574@gmail.com>
2024-02-25 17:24:40 +0000

    基礎的な環境を整えました。nuget, vector, umlを追加しました。

---
+++ CPMBase/Program.cs
@@ -0,0 +1,20 @@
+/*var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();*/
+
+using System.Numerics;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hallo World!!!");
+
+        Vector3 vector = Vector3.One;
+
+        Console.WriteLine();
+    }
+}
