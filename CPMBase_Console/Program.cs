821bc05379575cda4192d79c4cefe996672abca1
KotShinZ <s18574s18574@gmail.com>
2024-05-23 02:15:35 +0900

    Add Console Project

---
+++ CPMBase_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using CPMBase.Base;
+using CPMBase.Examples;
+using System.Reflection;
+
+
+public class Program
+{
+    public static void Main()
+    {
+        ((ISimration)new SimpleSim()).Run();
+    }
+}
